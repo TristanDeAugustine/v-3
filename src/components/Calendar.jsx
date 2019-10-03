6ab6b110377582045e99a80711eb63822edf7851
Tristan <deaugustinetristan@gmail.com>
2019-10-03 14:46:25 -0400

    dummy data

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,302 +1,71 @@
 import React from 'react'
-import moment from 'moment'
 import '../calendar.css'
 
-const style = {
-  position: 'relative',
-  margin: '50px auto'
-}
-
-class App extends React.Component {
-  onDayClick = (e, day) => {
-    alert(day)
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calendar
-          style={style}
-          width="302px"
-          onDayClick={(e, day) => this.onDayClick(e, day)}
-        />
-      </div>
-    )
-  }
-}
-export default class Calendar extends React.Component {
-  state = {
-    dateContext: moment(),
-    today: moment(),
-    showMonthPopup: false,
-    showYearPopup: false,
-    selectedDay: null
-  }
-
-  constructor(props) {
-    super(props)
-    this.width = props.width || '350px'
-    this.style = props.style || {}
-    this.style.width = this.width // add this
-  }
-
-  weekdays = moment.weekdays() //["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"]
-  weekdaysShort = moment.weekdaysShort() // ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-  months = moment.months()
-
-  year = () => {
-    return this.state.dateContext.format('Y')
-  }
-  month = () => {
-    return this.state.dateContext.format('MMMM')
-  }
-  daysInMonth = () => {
-    return this.state.dateContext.daysInMonth()
-  }
-  currentDate = () => {
-    console.log('currentDate: ', this.state.dateContext.get('date'))
-    return this.state.dateContext.get('date')
-  }
-  currentDay = () => {
-    return this.state.dateContext.format('D')
-  }
-
-  firstDayOfMonth = () => {
-    let dateContext = this.state.dateContext
-    let firstDay = moment(dateContext)
-      .startOf('month')
-      .format('d') // Day of week 0...1..5...6
-    return firstDay
-  }
-
-  setMonth = month => {
-    let monthNo = this.months.indexOf(month)
-    let dateContext = Object.assign({}, this.state.dateContext)
-    dateContext = moment(dateContext).set('month', monthNo)
-    this.setState({
-      dateContext: dateContext
-    })
-  }
-
-  nextMonth = () => {
-    let dateContext = Object.assign({}, this.state.dateContext)
-    dateContext = moment(dateContext).add(1, 'month')
-    this.setState({
-      dateContext: dateContext
-    })
-    this.props.onNextMonth && this.props.onNextMonth()
-  }
-
-  prevMonth = () => {
-    let dateContext = Object.assign({}, this.state.dateContext)
-    dateContext = moment(dateContext).subtract(1, 'month')
-    this.setState({
-      dateContext: dateContext
-    })
-    this.props.onPrevMonth && this.props.onPrevMonth()
-  }
-
-  onSelectChange = (e, data) => {
-    this.setMonth(data)
-    this.props.onMonthChange && this.props.onMonthChange()
-  }
-  SelectList = props => {
-    let popup = props.data.map(data => {
-      return (
-        <div key={data}>
-          <a
-            href="#"
-            onClick={e => {
-              this.onSelectChange(e, data)
-            }}
-          >
-            {data}
-          </a>
+const Calendar = () => {
+  return (
+    <>
+      <header className="header"> Calendar </header>
+      <container className="container3">
+        <div className="month">
+          <ul>
+            <li className="prev">&#10094;</li>
+            <li className="next">&#10095;</li>
+            <li>
+              August
+              <br />
+              <span styles="font-size:18px">2017</span>
+            </li>
+          </ul>
         </div>
-      )
-    })
 
-    return <div className="month-popup">{popup}</div>
-  }
+        <ul className="weekdays">
+          <li>Mo</li>
+          <li>Tu</li>
+          <li>We</li>
+          <li>Th</li>
+          <li>Fr</li>
+          <li>Sa</li>
+          <li>Su</li>
+        </ul>
 
-  onChangeMonth = (e, month) => {
-    this.setState({
-      showMonthPopup: !this.state.showMonthPopup
-    })
-  }
-
-  MonthNav = () => {
-    return (
-      <span
-        className="label-month"
-        onClick={e => {
-          this.onChangeMonth(e, this.month())
-        }}
-      >
-        {this.month()}
-        {this.state.showMonthPopup && <this.SelectList data={this.months} />}
-      </span>
-    )
-  }
-
-  showYearEditor = () => {
-    this.setState({
-      showYearNav: true
-    })
-  }
-
-  setYear = year => {
-    let dateContext = Object.assign({}, this.state.dateContext)
-    dateContext = moment(dateContext).set('year', year)
-    this.setState({
-      dateContext: dateContext
-    })
-  }
-  onYearChange = e => {
-    this.setYear(e.target.value)
-    this.props.onYearChange && this.props.onYearChange(e, e.target.value)
-  }
-
-  onKeyUpYear = e => {
-    if (e.which === 13 || e.which === 27) {
-      this.setYear(e.target.value)
-      this.setState({
-        showYearNav: false
-      })
-    }
-  }
-
-  YearNav = () => {
-    return this.state.showYearNav ? (
-      <input
-        defaultValue={this.year()}
-        className="editor-year"
-        ref={yearInput => {
-          this.yearInput = yearInput
-        }}
-        onKeyUp={e => this.onKeyUpYear(e)}
-        onChange={e => this.onYearChange(e)}
-        type="number"
-        placeholder="year"
-      />
-    ) : (
-      <span
-        className="label-year"
-        onDoubleClick={e => {
-          this.showYearEditor()
-        }}
-      >
-        {this.year()}
-      </span>
-    )
-  }
-
-  onDayClick = (e, day) => {
-    this.setState(
-      {
-        selectedDay: day
-      },
-      () => {
-        console.log('SELECTED DAY: ', this.state.selectedDay)
-      }
-    )
-
-    this.props.onDayClick && this.props.onDayClick(e, day)
-  }
-
-  render() {
-    // Map the weekdays i.e Sun, Mon, Tue etc as <td>
-    let weekdays = this.weekdaysShort.map(day => {
-      return (
-        <td key={day} className="week-day">
-          {day}
-        </td>
-      )
-    })
-
-    let blanks = []
-    for (let i = 0; i < this.firstDayOfMonth(); i++) {
-      blanks.push(
-        <td key={i * 80} className="emptySlot">
-          {''}
-        </td>
-      )
-    }
-
-    console.log('blanks: ', blanks)
-
-    let daysInMonth = []
-    for (let d = 1; d <= this.daysInMonth(); d++) {
-      let className = d == this.currentDay() ? 'day current-day' : 'day'
-      let selectedClass = d == this.state.selectedDay ? ' selected-day ' : ''
-      daysInMonth.push(
-        <td key={d} className={className + selectedClass}>
-          <span
-            onClick={e => {
-              this.onDayClick(e, d)
-            }}
-          >
-            {d}
-          </span>
-        </td>
-      )
-    }
-
-    console.log('days: ', daysInMonth)
-
-    var totalSlots = [...blanks, ...daysInMonth]
-    let rows = []
-    let cells = []
-
-    totalSlots.forEach((row, i) => {
-      if (i % 7 !== 0) {
-        cells.push(row)
-      } else {
-        let insertRow = cells.slice()
-        rows.push(insertRow)
-        cells = []
-        cells.push(row)
-      }
-      if (i === totalSlots.length - 1) {
-        let insertRow = cells.slice()
-        rows.push(insertRow)
-      }
-    })
-
-    let trElems = rows.map((d, i) => {
-      return <tr key={i * 100}>{d}</tr>
-    })
-
-    return (
-      <div className="calendar-container" style={this.style}>
-        <table className="calendar">
-          <thead>
-            <tr className="calendar-header">
-              <td colSpan="5">
-                <this.MonthNav /> <this.YearNav />
-              </td>
-              <td colSpan="2" className="nav-month">
-                <i
-                  className="prev fa fa-fw fa-chevron-left"
-                  onClick={e => {
-                    this.prevMonth()
-                  }}
-                ></i>
-                <i
-                  className="prev fa fa-fw fa-chevron-right"
-                  onClick={e => {
-                    this.nextMonth()
-                  }}
-                ></i>
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>{weekdays}</tr>
-            {trElems}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+        <ul className="days">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>
+            <span className="active">10</span>
+          </li>
+          <li>11</li>
+          <li>12</li>
+          <li>13</li>
+          <li>14</li>
+          <li>15</li>
+          <li>16</li>
+          <li>17</li>
+          <li>18</li>
+          <li>19</li>
+          <li>20</li>
+          <li>21</li>
+          <li>22</li>
+          <li>23</li>
+          <li>24</li>
+          <li>25</li>
+          <li>26</li>
+          <li>27</li>
+          <li>28</li>
+          <li>29</li>
+          <li>30</li>
+          <li>31</li>
+        </ul>
+      </container>
+    </>
+  )
 }
+
+export default Calendar
